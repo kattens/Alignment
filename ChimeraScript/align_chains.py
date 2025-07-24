b21fa37907cee632f5a461774ad185f2e1e099b0
McKenshel Cameau <mcameau20@gmail.com>
2025-07-24 11:52:10 -0400

    made adjustments for paths in ChimeraScript

--- ChimeraScript/align_chains.py
+++ ChimeraScript/align_chains.py
@@ -1,26 +1,30 @@
 from chimerax.core.commands import run
 import csv
-
+import os
 count=0
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+print(base_dir)
+
 
 # method for structral alignment 
 def run_alignment(session, target, malaria, target_chain, malaria_chain, count):
     run(session,"log clear")
 
     # Loads  PDBs at specified path
-    run(session, f'open C:/malaria_align_project/pdbs/{target}')
-    run(session, f"open C:/malaria_align_project/pdbs/{malaria}")
+    run(session, f'open {base_dir}/pdbs/{target}')
+    run(session, f"open {base_dir}/pdbs/{malaria}")
     
     # alignment by specific chain
     run(session, f"matchmaker #2/{malaria_chain} to #1/{target_chain}")
     
-    run(session, f"log save C:/malaria_align_project/logs/output{count}.txt")
+    run(session, f"log save {base_dir}/logs/output{count}.txt")
 
     run(session,"close all")
 
 
 def main(session):
-    input_csv = "C:/malaria_align_project/malaria_mapping.csv"
+    input_csv = f"{base_dir}/malaria_mapping.csv"
     count = 0
 
     with open(input_csv, newline='') as csvfile:
